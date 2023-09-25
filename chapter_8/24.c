1a9328be4f2defd4f22c7991c42911894f29682d
Tomer Sela <tomersela@gmail.com>
2023-09-25 19:19:30 +0300

    Solution for chapter 8 problem 24

---
+++ chapter_8/24.c
@@ -0,0 +1,37 @@
+#include "csapp.h"
+#define N 2
+#define LINE_LENGTH 80
+
+int main() 
+{
+    int status, i;
+    pid_t pid;
+    int* null_ptr = NULL;
+
+    /* Parent creates N children */
+    for (i = 0; i < N; i++)
+        if ((pid = Fork()) == 0) { /* Child */
+            *null_ptr = 1; // segmentation fault
+        }
+
+    /* Parent reaps N children in no particular order */
+    while ((pid = waitpid(-1, &status, 0)) > 0) {
+        if (WIFEXITED(status))
+            printf("child %d terminated normally with exit status=%d\n",
+                pid, WEXITSTATUS(status));
+        else if (WIFSIGNALED(status)) { // terminated because of a signal
+            char buf[LINE_LENGTH];
+            int code = WTERMSIG(status);
+            sprintf(buf, "child %d terminated by signal %d", pid, code);
+            psignal(code, buf);
+        }
+        else
+            printf("child %d terminated abnormally\n", pid);
+    }
+
+    /* The only normal termination is if there are no more children */
+    if (errno != ECHILD)
+	    unix_error("waitpid error");
+
+    exit(0);
+}
